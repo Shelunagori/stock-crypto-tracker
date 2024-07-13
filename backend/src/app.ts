6de2d627084f969b661d65240eb7ee2f22d24c0d
Shelunagori <shelunagori@gmail.com>
2024-07-13 11:41:59 +0530

    backend file and folder with health-check api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,4 +2,9 @@ import express from 'express';
 const app = express();
 // Middleware
 app.use(express.json());
+
+app.get("/health-check", (req, res) => {
+    res.json("ALL GOOD");
+});
+
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,5 @@
 import app from './app';
 
-app.get("/health-check", (req, res) => {
-    res.json("ALL GOOD");
-})
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
